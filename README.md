5ab837a40902abb53868d395f169cbcbb4e0e6b5
Muhammad Aljafar <xqg1122@gmail.com>
2024-07-04 21:16:03 +0300

    Finish day 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# My 2023 Advent of Code solutions
+in go, lang.
+# advent-of-code-2023
---
+++ day1/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	sum := 0
+	for _, line := range readInputFile() {
+		sum += getCalibrationValue(line)
+	}
+	fmt.Println("Sum:", sum)
+}
+
+func getCalibrationValue(text string) int {
+	digitNames := map[string]rune{
+		"one":   '1',
+		"two":   '2',
+		"three": '3',
+		"four":  '4',
+		"five":  '5',
+		"six":   '6',
+		"seven": '7',
+		"eight": '8',
+		"nine":  '9',
+	}
+
+	// ? means unassigned
+	leftMost, rightMost := '?', '?'
+
+	assignToCorrectVariable := func(char rune) {
+		if leftMost == '?' {
+			leftMost, rightMost = char, char
+		} else {
+			rightMost = char
+		}
+	}
+
+	for i, char := range text {
+		if isDigit(char) {
+			assignToCorrectVariable(char)
+			continue
+		}
+
+		for digitName := range digitNames {
+			if len(text[i:]) < len(digitName) {
+				continue
+			}
+
+			found := true
+			for j := range digitName {
+				if text[i+j] != digitName[j] {
+					found = false
+					break
+				}
+			}
+			if found {
+				assignToCorrectVariable(digitNames[digitName])
+			}
+		}
+	}
+
+	// sees if the text has no digits at all by checking leftMost
+	if leftMost == '?' {
+		return 0
+	}
+
+	finalString := string(leftMost) + string(rightMost)
+	fmt.Println(finalString)
+
+	calibVal, _ := strconv.Atoi(finalString)
+	return calibVal
+}
+
+func readInputFile() []string {
+	contents, err := os.ReadFile("./input.txt")
+	if err != nil {
+		log.Fatal("Error reading the input file")
+	}
+
+	lines := strings.Split(string(contents), "\n")
+
+	return lines
+}
+
+func isDigit(char rune) bool {
+	return '0' <= char && char <= '9'
+}
